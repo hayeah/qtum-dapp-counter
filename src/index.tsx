c5121ea1bdf78281d326ba9e60b339ee21457388
Howard Yeh <howard@metacircus.com>
2017-11-02 21:46:57 +0800

    add font-awesome

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
+import "font-awesome/css/font-awesome.css"
 import "normalize.css"
+
 import "./index.css"
 
 import * as React from "react"
 import { render } from "react-dom"
 
-render(<h1>Hello React TypeScript!</h1>, document.getElementById("root"))
+render(
+  <h1>
+    Hello React TypeScript <span className="fa fa-heart" />
+  </h1>,
+  document.getElementById("root"))
